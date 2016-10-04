a7d44b946608c10a8f6a1cb2d5e9288e113cd099
Jeff Kealy <jeffkealy@MacBook-Pro.local>
2016-10-04 11:47:29 -0500

    added function to js file

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hey! this works!");
+function hey (a,b) {
+    
+    var x = a + b;
+    return (x);
+}
+  
+var done = hey(1,1);
+ if (done = 2){
+        
+        console.log("hey! this works!");
+        }
